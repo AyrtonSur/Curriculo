2be1a93ee4448be13ecf27a2fe859ae26c1a5394
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-07 00:41:22 -0300

    feat: criação de script para animação e para o botões nas habilidades

---
+++ scripts/script.js
@@ -0,0 +1,99 @@
+let typeWriterTimeout
+
+function typeWriter(text, element, delay = 20) {
+  element.textContent = ''
+  let i = 0
+
+  function type() {
+    if (i < text.length) {
+      element.textContent += text.charAt(i)
+      i++
+      typeWriterTimeout = setTimeout(type, delay)
+    }
+  }
+
+  clearTimeout(typeWriterTimeout)
+  type()
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const buttons = document.querySelectorAll('.divAbilities button')
+
+  buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const abilityName = button.querySelector('.abilityName').textContent
+      const abilityText = document.querySelector('#abilityText')
+
+      buttons.forEach(btn => {
+        const img = btn.querySelector('img')
+        if (img) {
+          img.classList.remove('curAbility')
+        }
+      });
+
+      const img = button.querySelector('img')
+      if (img) {
+        img.classList.add('curAbility')
+      }
+
+      let text = ''
+      switch (abilityName) {
+        case 'Python':
+          text = 'Tenho conhecimento básico de Python e experiência com algumas bibliotecas, como Numpy e Pygame.'
+          break
+        case 'Java':
+          text = 'Conhecimento básico de Java, já fiz um único jogo na linguagem, além de ter estudado sobre a linguagem para a faculdade.'
+          break
+        case 'TypeScript':
+          text = 'Uso TypeScript para desenvolvimento de aplicações back-end, atualmente é a linguagem que mais utilizo, além de ser a que tenho mais experiência prática. Já fiz alguns projetos na linguagem.'
+          break
+        case 'JavaScript':
+          text = 'JavaScript é uma linguagem que tenho grande conhecimento sobre. Já tive experiência fazendo alguns projetos em JavaScript. Esses projetos utilizaram principalmente: Sequelize, SQLite e Express.'
+          break
+        case 'C':
+          text = 'Tenho conhecimento em C por causa da faculdade, já fiz alguns programas simples e algumas estruturas na linguagem, como árvores B ou árvore binárias de busca.'
+          break
+        case 'Rust':
+          text = 'Rust é uma das minhas linguagens favoritas, porém ainda não tenho tanto conhecimento nela, quanto nas outras linguagens. Atualmente estou tentando estudar mais sobre a linguagem para, futuramente, começar a fazer projetos interessantes dentro dela.'
+          break
+        case 'Node.Js':
+          text = 'Uso Node.js para desenvolvimento de servidores e aplicações back-end, sendo minha principal ferramenta para tal.'
+          break
+        case 'React':
+          text = 'Tenho um bom conhecimento de React, porém estou mais acostumado a utilizar o próprio HTML, do que o React.'
+          break
+        case 'DataBase':
+          text = 'Tenho experiência com bancos de dados relacionais, sabendo um pouco sobre MySQL e MariaDB, porém minhas principais experiências com Banco de Dados foram utilizando ORMs.'
+          break
+        case 'HTML':
+          text = 'Tenho bom conhecimento em HTML para construção de páginas web estruturadas e semânticas.'
+          break
+        case 'CSS':
+          text = 'Conhecimento intermediário de CSS, conseguindo estilizar páginas de forma que fiquem bem estruradas.'
+          break
+        default:
+          text = 'Selecione uma habilidade para ver mais detalhes.'
+      }
+
+      typeWriter(text, abilityText)
+    })
+  })
+})
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const textElement = document.querySelector('.animated-text')
+  const text = textElement.textContent
+  textElement.innerHTML = ''
+
+  for (let i = 0; i < text.length; i++) {
+    const span = document.createElement('span')
+    if (text[i] === ' ') {
+      span.innerHTML = '&nbsp;'
+    } else {
+      span.textContent = text[i]
+    }
+    span.style.animationDelay = `${i * 0.1}s`
+    textElement.appendChild(span)
+  }
+})
