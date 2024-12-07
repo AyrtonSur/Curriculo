a31d7faa52ec8b0e7f9686c3a6f7a4fdf583207f
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-07 01:34:13 -0300

    feat: fazendo script pro menu

--- scripts/script.js
+++ scripts/script.js
@@ -97,3 +97,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     textElement.appendChild(span)
   }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger')
+  const navMenu = document.getElementById('nav-menu')
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+  })
+})
